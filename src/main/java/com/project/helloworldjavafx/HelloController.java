f1d0c0e70f5c0f869f6b77b0db54f8859e178125
Caio Bomfim <caaiobomfim@gmail.com>
2023-11-25 16:06:26 -0300

    [PROJECT] - MainView controller

--- src/main/java/com/project/helloworldjavafx/HelloController.java
+++ src/main/java/com/project/helloworldjavafx/HelloController.java
@@ -1,15 +1,38 @@
 package com.project.helloworldjavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    private Button btnTest;
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
 
     @FXML
-    public void onBtnTestAction() {
-        System.out.println("CLICK");
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
