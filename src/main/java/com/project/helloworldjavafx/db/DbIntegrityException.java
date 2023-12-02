ad02a394224a6f34173d60a8a411eec29b15fe6d
Caio Bomfim <caaiobomfim@gmail.com>
2023-12-02 16:47:29 -0300

    [PROJECT] - Finalização do projeto

--- src/main/java/com/project/helloworldjavafx/db/DbIntegrityException.java
+++
@@ -1,9 +0,0 @@
-package com.project.helloworldjavafx.db;
-
-public class DbIntegrityException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DbIntegrityException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/project/helloworldjavafx/listeners/DataChangeListener.java
+++
@@ -1,6 +0,0 @@
-package com.project.helloworldjavafx.listeners;
-
-public interface DataChangeListener {
-
-    void onDataChanged();
-}
